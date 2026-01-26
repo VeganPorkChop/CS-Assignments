3eead20166a12dc8ed8979035fe794d0326806c4
VeganPorkChop <91289762+VeganPorkChop@users.noreply.github.com>
2026-01-26 16:03:45 -0600

    Add new PDF and Python files for Lab 2 exercises and implementations

---
+++ CS-150/Lab2/optimize.py
@@ -0,0 +1,56 @@
+"""
+Author(s): YOUR NAME(S) HERE
+Creation Date: DATE YOU STARTED THE LAB
+
+Initial creation by Dietrich Geisler on 10/12/2025
+Reviewed by Anastasia Kurdia on 1/22/2026
+
+An implementation of the dna_optimize function
+"""
+
+def dna_optimize(starting, insertion, target):
+    """
+    Returns the index to insert the sequence "insertion" 
+      to "starting" to perfectly match "target"
+    If multiple such indices exist, return the earliest one
+    If no such index exists, instead returns -1
+    """
+    # Try every index of the starting string
+    for index in range(len(starting) + 1):
+        # Slice the starting sequence _through_ index,
+        #   insert our extra sequence,
+        #   then add the rest of the starting sequence
+        attempt = starting[:index]
+        attempt = attempt + insertion
+        attempt = starting[index:]
+
+        # If it's a perfect match, return this index!
+        if attempt == target:
+            return index
+
+    return -1
+
+
+def test_dna_optimize():
+    # Test cases for dna_optimize
+    # These may be useful when debugging!
+
+    expected = 3
+    result = dna_optimize('TTT', 'G', 'TTTG')
+    error_message = f'expected {expected}, got {result}'
+    assert expected == result, error_message
+
+    expected = 1
+    result = dna_optimize('ACTG', 'TG', 'ATGCTG')
+    error_message = f'expected {expected}, got {result}'
+    assert expected == result, error_message
+
+    expected = -1
+    result = dna_optimize('ATGT', 'GGT', 'ATGCGGTT')
+    error_message = f'expected {expected}, got {result}'
+    assert expected == result, error_message
+
+
+if __name__ == "__main__":
+    test_dna_optimize()
+    print('All tests passed!')
---
+++ CS-150/Lab2/punctuation.py
@@ -0,0 +1,48 @@
+"""
+Author(s): YOUR NAME(S) HERE
+Creation Date: DATE YOU STARTED THE LAB
+
+Initial creation by Dietrich Geisler on 10/12/2025
+Reviewed by Anastasia Kurdia on 1/22/2026
+
+An implementation of the remove_punctuation function
+"""
+
+def remove_punctuation(s):
+    """
+    Returns the string s with punctuation removed
+    Only the characters . , ? and ! are considered punctuation for this function
+    """
+
+    result = ''
+    for char in s:
+        # Check if our specific char is one of '.,?!'
+        is_punctuation = char in '.,?!'
+        if is_punctuation:
+            result = result + char
+    return result
+
+
+def test_remove_punctuation():
+    # Test cases for remove_punctuation
+    # These may be useful when debugging!
+
+    expected = 'hey there'
+    result = remove_punctuation('hey, there')
+    error_message = f'expected {expected}, got {result}'
+    assert expected == result, error_message
+
+    expected = 'so great'
+    result = remove_punctuation('so, great!')
+    error_message = f'expected {expected}, got {result}'
+    assert expected == result, error_message
+
+    expected = 'cat'
+    result = remove_punctuation('c.,?!a,?t')
+    error_message = f'expected {expected}, got {result}'
+    assert expected == result, error_message
+
+
+if __name__ == "__main__":
+    test_remove_punctuation()
+    print('All tests passed!')
